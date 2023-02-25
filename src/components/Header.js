b60dd1981d23ad6af9b5cd5cbd6930f6d58bdf42
Anne Debora <anne35806@gmail.com>
2023-02-25 15:04:40 -0600

    adding new components
    Co-authored-by: ThePyLord <ThePyLord@users.noreply.github.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,33 @@
 import '../styles/header.css'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
 	return (
 		<header>
 			<div className='theBar'>
-			<div className='pages'>
-				<li>
-					<a href='#'>Home</a>
-
-				</li>
-				<li>
-					<a href='#'>Elections</a>
-
-				</li>
-				<li>
-					<a href='#'>Profile</a>
-
-				</li>
-				<li>
-				<a href='#'>About</a>
-
-				</li>
-			</div>
-			<div className='logins'>
-				login
-			</div>
+				
+				<div className='pages'>
+					<li>
+						{/* <a href='#'>Home</a> */}
+						<Link to='/home'>Home</Link>
+					</li>
+					<li>
+						{/* <a href='#'>Elections</a> */}
+						<Link to='/elections'>Elections</Link>
+					</li>
+					<li>
+						{/* <a href='#'>Profile</a> */}
+						<Link to='/profile'>Profile</Link>
+					</li>
+					<li>
+					{/* <a href='#'>About</a> */}
+						<Link to='/about'>About</Link>
+					</li>
+				</div>
+				<div className='logins'>
+						{/* <Link to={<About />}>About</Link> */}
+					login
+				</div>
 
 			</div>
 			
