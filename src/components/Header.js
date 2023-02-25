317d986eea390e9d8f63314c6f25732d85035336
Onyelechie <onyelechieebere@gmail.com>
2023-02-25 11:31:45 -0600

    added app title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@
 export default function Header() {
 	return (
 		<header>
-			<h1>My React App</h1>
+			<h1>The Voting Range</h1>
 			<p>Created alongside Dr. Eberechukwu(Creative Director), Dr. Niyonkad(Chief Executive Director)</p>
 		</header>
 	)
