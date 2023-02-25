969aa17e3a068bbfe1f86fdbea82920a3bd767f6
Onyelechie <onyelechieebere@gmail.com>
2023-02-25 16:52:31 -0600

    Sign Up changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-
+import Form from './components/Form';
 function App() {
   return (
     <div className="App">
@@ -19,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <Header />
+      <Form />
     </div>
   );
 }
