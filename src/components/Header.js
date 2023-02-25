b0c6af4aea2195845e6e90c46c5029c8a49d61ef
ThePyLord <geraldfeu@gmail.com>
2023-02-25 11:26:01 -0600

    Added header and styles folder.

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+	return (
+		<header>
+			<h1>My React App</h1>
+			<p>Created alongside Dr. Eberechukwu(Creative Director), Dr. Niyonkad(Chief Executive Director)</p>
+		</header>
+	)
+}
