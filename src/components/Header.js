8be83d1600957b4a10aa0ef631e2da42f1d87d0c
Anne Debora <anne35806@gmail.com>
2023-02-25 12:50:01 -0600

    styling the header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,32 @@ import '../styles/header.css'
 export default function Header() {
 	return (
 		<header>
-			<h1>The Voting Range</h1>
+			<div className='theBar'>
+			<div className='pages'>
+				<li>
+					<a href='#'>Home</a>
+
+				</li>
+				<li>
+					<a href='#'>Elections</a>
+
+				</li>
+				<li>
+					<a href='#'>Profile</a>
+
+				</li>
+				<li>
+				<a href='#'>About</a>
+
+				</li>
+			</div>
+			<div className='logins'>
+				login
+			</div>
+
+			</div>
+			
+			<h1><b>The Voting Range</b></h1>
 			<p>Created alongside Dr. Eberechukwu(Creative Director), Dr. Niyonkad(Chief Executive Director)</p>
 		</header>
 	)
